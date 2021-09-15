9047a44953b8ce1b63093e02f6777c3479ce0ea1
Akonaay <ambrosesulley4@gmail.com>
2021-09-15 16:18:33 +0300

    Bras page and link activation updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,20 @@ $('.owl-carousel').owlCarousel({
     // nav: true
 })
 
+$('.portfolio-popup').magnificPopup({
+    type: 'image',
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function (openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
+
